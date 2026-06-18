6e72d1be39c41824a64282874c7158b532d51b01
meghanaambati452-blip <meghanaambati452@gmail.com>
2026-06-18 13:39:59 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleTheme() {
-  const current = document.documentElement.getAttribute("data-theme");
-
-  if (current === "dark") {
-    document.documentElement.removeAttribute("data-theme");
-  } else {
-    document.documentElement.setAttribute("data-theme", "dark");
-  }
-}
